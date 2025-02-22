b9d44193d9a114e7f4391768bd34b4acce6aac30
ksumarshmallow <ksumarshmallow2020@gmail.com>
2025-02-22 09:40:59 +0300

    multople js

--- public/main.js
+++ public/main.js
@@ -1,8 +1,8 @@
-import { toggleListVisibility } from './toggle.js';
-import { showMoreButton } from './showMore.js';
-import { initializeRangeSlider } from './rangeSlider.js';
-import { initializeSearch } from './search.js';
-import { loadCSVData, filterReviews, resetFilters } from './dataloader.js';
+import { toggleListVisibility } from './scripts/toggle.js';
+import { showMoreButton } from './scripts/showMore.js';
+import { initializeRangeSlider } from './scripts/rangeSlider.js';
+import { initializeSearch } from './scripts/search.js';
+import { loadCSVData, filterReviews, resetFilters } from './scripts/dataloader.js';
 
 document.addEventListener('DOMContentLoaded', function () {
     loadCSVData();

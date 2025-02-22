a41386f984ad726cc42a1e6c8613faaff4dcc452
ksumarshmallow <ksumarshmallow2020@gmail.com>
2025-02-22 08:55:29 +0300

    npm server start

---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const path = require('path');
+const fs = require('fs');
+const Papa = require('papaparse');
+const cors = require('cors');
+
+const app = express();
+const port = 3000;
+
+app.use(cors());
+
+// add static files
+app.use(express.static(path.join(__dirname, 'public')));
+
+// start server
+app.listen(port, () => {
+    console.log(`Сервер запущен на http://localhost:${port}`);
+});
